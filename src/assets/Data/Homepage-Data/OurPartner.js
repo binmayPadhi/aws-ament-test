4dab0e37af812f7df5d406a0ab0f106d9cec74dd
binmayPadhi <binmaykumarpadhi@gmail.com>
2021-02-10 14:46:28 +0530

    nifi logo removed

--- src/assets/Data/Homepage-Data/OurPartner.js
+++ src/assets/Data/Homepage-Data/OurPartner.js
@@ -2,7 +2,7 @@ import microsoftLogo from "../../images/HomePage/partner-logos/microsoft.png";
 import googleLogo from "../../images/HomePage/partner-logos/google.png";
 import talendLogo from "../../images/HomePage/partner-logos/talend.png";
 import hadoopLogo from "../../images/HomePage/partner-logos/hadoop.png";
-import nifiLogo from "../../images/HomePage/partner-logos/nifi.png";
+// import nifiLogo from "../../images/HomePage/partner-logos/nifi.png";
 import siliconLogo from "../../images/HomePage/partner-logos/silicon.png";
 import teamleaseLogo from "../../images/HomePage/partner-logos/teamlease.png";
 import cioreviewLogo from "../../images/HomePage/partner-logos/cioreview.png";
@@ -24,10 +24,10 @@ const ourPartnerData = [
         id : 4,
         partnerLogo : hadoopLogo
     },
-    {
-        id : 5,
-        partnerLogo : nifiLogo
-    },
+    // {
+    //     id : 5,
+    //     partnerLogo : nifiLogo
+    // },
     {
         id : 6,
         partnerLogo : siliconLogo
