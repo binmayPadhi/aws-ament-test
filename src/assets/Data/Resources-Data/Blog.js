bc6d56c487fe9f2539fc7acc56aa664794ff356b
Manikanta Kumar Nalam <manikanta@amnetdigital.com>
2022-10-31 16:48:18 +0530

    adding resources page to current website

---
+++ src/assets/Data/Resources-Data/Blog.js
@@ -0,0 +1,55 @@
+
+
+const blog = [
+    {
+        id: 1,
+        title: "Seven Significant ways Artificial Intelligence (AI) is Impacting the Retail Industry",
+        description: "Artificial Intelligence is becoming the limelight practice for Retail businesses as online shopping replaces more and more physical establishments. In today's fast evolution of digitalization, Retailers are using artificial intelligence technologies to help them match their products and services to the expectations of their customers in the digital age when customers are continuously looking for bespoke goods and services. To master productivity and efficiency operations, Artificial Intelligence is becoming the most powerful tool.",
+        description1: "Worldwide artificial intelligence (AI) software revenue is forecast to total $62.5 billion in 2022, an increase of 21.3% from 2021, according to a new forecast from Gartner, Inc."
+    },
+    {
+        id: 2,
+        title: "Inflating In-store Experience",
+        description: "One of the biggest ways AI is transforming the retail industry is by automating tasks that were previously completed by human workers. As a result, your personnel may engage more in handling difficult customers' problems and less in laborious activities. Potential to materialize real-time stock monitoring, digitizing retail displays and trial rooms, and replenishing inventory, to serve consumers in the business and shorten lines by accepting payments without a cashier. For retailers who wish to maximize their time and enhance customer experience, I recommend AI.",
+    },
+    {
+        id: 3,
+        title: "Customer Service Assistance through Conversational AI/Digital Assistant/Virtual Assistant",
+        description: "Customer interaction is ensured by omnichannel retail so that it doesn't begin and end at a location. By developing interactive chat programs, artificial intelligence solutions in retail may easily increase client engagement. Chatbots can be useful tools for consumer communication. AI-powered assistants that can deliver specialized shopping assistance and support to customers at scale and utilize natural language will play a significant role in this.They can respond to frequently asked questions, make product recommendations, address complaints, and gather insightful information from your consumers before transferring the call, if necessary, to a telesales executive.They offer a strong customer experience and are open round- the - clock",
+    },
+    {
+        id: 4,
+        title: "Price Adjustment Flexibility",
+        description: "These potentials could lead to not only cost savings but also improvements in customer happiness, retention, and overall experience—all of which are critical in the retail industry. Another area where AI has advanced quickly is payments. AI not only makes payments easier but also prevents fraud. To proactively find and stop transaction fraud, PayPal's AI utilizes a deep learning model built on years of digital transactions. Retailers can also change their prices in response to seasonal patterns, rival items, and customer demand. It is undoubtedly a worthwhile investment when we consider the potential long-term revenue it can produce.",
+    },
+
+    {
+        id: 5,
+        title: "Constructive Logistics and Supply Chain",
+        description: "AI also has the caliber to streamline your in-store supply chain. For instance, this technology can analyze the purchasing patterns of your previous clients and notify you in advance when you will run out of specific products. This is important since clients may choose a different business if they don't feel certain that you'll always have the products they require. And that means you'll lose sales, which will hurt your bottom line.AI can also forecast when a particular product will be in more demand throughout the year. As a result, you can start stocking up now rather than rushing to secure and display these popular seasonal items when clients start asking about them.",
+    },
+    {
+        id: 6,
+        title: "Personalization, Customer Understanding, and Recommendation System",
+        description: "Both customers and businesses are placing more and more value on tailored recommendations and purchase predictions. Retail artificial intelligence can provide customers with a personalized buying experience. Technologies like fingerprint and face recognition can recognize returning clients and keep track of their preferences. In order to understand consumers' preferences, cutting-edge AI algorithms can examine their demographic information, social media impressions, and digital imprints. According to statistics, 71% of users won't even open an impersonalised email. Additionally, companies claim that targeted campaigns raised their postal earnings by 760%. E-commerce and retail businesses leverage machine learning to recommend relevant products to clients based on their interests, searches, and past purchases.",
+    },
+    {
+        id: 7,
+        title: "Robotic Product Searches",
+        description: "Additionally, artificial intelligence in retail helps assist customers in being able to find products by taking pictures of a product they like in the real world and using that photo to look up online retailers selling that good. In just a few seconds, machine learning retail software can categorize millions of products from diverse vendors for clients, making product comparison and search simpler than ever.",
+    },
+    {
+        id: 8,
+        title: "Automating the Warehouse",
+        description: "The complexity of store operations is a result of multichannel retail. In a warehouse, store employees need to perform a variety of tasks. Their duties now extend beyond simply receiving, stocking, and controlling inventory. They need to manage marketing, track promotional offers, and provide product information. A short examination of the products delivered by AI in warehouses and retail supply chains reveals pricing irregularities and out-of-stock items. It can take a while to realize a product has run out at large warehouses. Smart shelves are used in an AI- based inventory management system to notify personnel when there are product shortages or missing items in the inventory.The time saved can be successfully put to use for other fruitful tasks, which will cut down on the amount of time spent at the warehouse.",
+    },
+    {
+        id: 9,
+        title: "AI - The future buzz for business continuity and growth",
+        description: "AI has the power to completely change the retail industry. The retailers who can develop their retail channels will distinguish themselves as industry leaders when digital and physical purchase channels merge. It offers retailers a future-oriented perspective and takes the place of intuition with intelligence. When implementing AI, business executives must be practical in their approach.AI will help merchants comprehend customer psychology by bringing us closer to the current state of the human brain. A new AI algorithm can actually understand what someone is thinking by using deep learning networks. Last but not least, future research can concentrate on creating AI models to better comprehend consumer/shopper ideas using. For this study, researchers from several fields, including marketing, information systems, computer science, statistics, economics, and psychology, must work together and engage in cross-functional thinking.",
+    },
+]
+
+export default blog;
+
+
--- src/assets/components/Homepage.jsx
+++ src/assets/components/Homepage.jsx
@@ -1,5 +1,5 @@
-import React,{ Suspense } from "react";
-import {Link} from "react-scroll";
+import React, { Suspense } from "react";
+import { Link } from "react-scroll";
 import HomeBG from "../images/HomePage/home-bg.jpg";
 import SolutionsServices from "../components/Homepage/SolutionsServices";
 // import ClientSay from "../components/Homepage/ClientSay";
@@ -10,7 +10,7 @@ import OurPartner from "../components/Homepage/OurPartner";
 
 // import IndusrtySection from "../components/Homepage/IndustrySection";
 
-function Test (){
+function Test() {
 
     const cookieStorage = {
         getItem: (item) => {
@@ -24,14 +24,14 @@ function Test (){
             document.cookie = `${item}=${value};`
         }
     }
-    
+
     const storageType = cookieStorage;
     const consentPropertyName = 'jdc_consent';
     const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
     const saveToStorage = () => storageType.setItem(consentPropertyName, true);
-    
+
     window.onload = () => {
-    
+
         const acceptFn = event => {
             saveToStorage(storageType);
             consentPopup.classList.add('hidden');
@@ -44,37 +44,37 @@ function Test (){
         const declineBtn = document.getElementById("decline");
         acceptBtn.addEventListener('click', acceptFn);
         declineBtn.addEventListener("click", declineFn);
-    
+
         if (shouldShowPopup(storageType)) {
             setTimeout(() => {
                 consentPopup.classList.remove('hidden');
             }, 2000);
         }
-    
+
     };
 
     const ClientSay = React.lazy(() => import('../components/Homepage/ClientSay'));
     const CaseStudiesSection = React.lazy(() => import('../components/Homepage/CaseStudiesSection'));
     const IndusrtySection = React.lazy(() => import('../components/Homepage/IndustrySection'));
 
-    return(<>
-            <div className="sub-service-intro container-fluid" style={{backgroundImage:`url(${HomeBG})`,backgroundAttachment:"scroll",backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
-                         <div className="row service-new-all-margin">
-                        <div className="col-lg-5 new-subService-AIML-intro-caption new-all-margin">
-                            <h2 className="textAnimation" data-text="Reimagine & Redefine">
-                            Reimagine & Redefine
-                            </h2>
-                            <p>
-                            Amnet Digital is enabling enterprises reimagine and redefine their engagement with 
-                            employees, customers, partners and suppliers by providing intelligent 
-                            insights and continuous value.
-                            </p>
+    return (<>
+        <div className="sub-service-intro container-fluid" style={{ backgroundImage: `url(${HomeBG})`, backgroundAttachment: "scroll", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+            <div className="row service-new-all-margin">
+                <div className="col-lg-5 new-subService-AIML-intro-caption new-all-margin">
+                    <h2 className="textAnimation" data-text="Reimagine & Redefine">
+                        Reimagine & Redefine
+                    </h2>
+                    <p>
+                        Amnet Digital is enabling enterprises reimagine and redefine their engagement with
+                        employees, customers, partners and suppliers by providing intelligent
+                        insights and continuous value.
+                    </p>
 
-                            <div className="subService-AIML-intro-btn">
-                                {/* <a href="/casestudies">
+                    <div className="subService-AIML-intro-btn">
+                        {/* <a href="/casestudies">
                                     <button type="button" className="btn btn-default">View Details</button>
                                 </a> */}
-                            <Link
+                        <Link
                             activeClass="active"
                             to="details"
                             spy={true}
@@ -83,64 +83,64 @@ function Test (){
                             duration={500}>
                             <button type="button" className="btn btn-default sub-service-view-details-btn">Learn more</button>
                         </Link>
-                            </div>
-                        </div>
-                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
 
-            {/* HOMEPAGE QUOTE SECTION */}
-            <div className="homepage-quote homepage-news-text">
-                <h2 className="text-center">
-                    "AI-Enabled tools will generate $2.9 trillion in business value by 2021"
-                </h2>
-            </div>
+        {/* HOMEPAGE QUOTE SECTION */}
+        <div className="homepage-quote homepage-news-text">
+            <h2 className="text-center">
+                "AI-Enabled tools will generate $2.9 trillion in business value by 2021"
+            </h2>
+        </div>
 
 
-            {/* NEW HOMEPAGE SOLUTION SERVICE SECTION */}
-            <SolutionsServices />
+        {/* NEW HOMEPAGE SOLUTION SERVICE SECTION */}
+        <SolutionsServices />
 
-            {/* NEW HOMEPAGE SOLUTION SERVICE SECTION */}
-            <AboutDetails />
+        {/* NEW HOMEPAGE SOLUTION SERVICE SECTION */}
+        <AboutDetails />
 
-            {/* NEW HOMEPAGE INDUSTRIES SECTION */}
-            <Suspense fallback={<div>Loading...</div>}>
+        {/* NEW HOMEPAGE INDUSTRIES SECTION */}
+        <Suspense fallback={<div>Loading...</div>}>
             <IndusrtySection />
-            </Suspense>
-            
+        </Suspense>
 
-            {/* NEW HOMEPAGE OUR SERVICE SECTION */}
-            <OurServicesSection />
 
-            {/* NEW HOMEPAGE CASESTUDIES SECTION */}
-            <Suspense fallback={<div>Loading...</div>}>
+        {/* NEW HOMEPAGE OUR SERVICE SECTION */}
+        <OurServicesSection />
+
+        {/* NEW HOMEPAGE CASESTUDIES SECTION */}
+        <Suspense fallback={<div>Loading...</div>}>
             <CaseStudiesSection />
-            </Suspense>
-            
+        </Suspense>
 
-            {/* NEW HOMEPAGE OUR PARTNER SECTION */}
-            <div className="new-our-partner">
-                <h1 className="section-title mt-3 text-center">RECOGNITIONS & PARTNERSHIPS</h1> 
-                <OurPartner />
-            </div>
 
-            {/* NEW HOMEPAGE CLIENT SAY */}
-            <Suspense fallback={<div>Loading...</div>}>
-                <ClientSay />
-            </Suspense>
+        {/* NEW HOMEPAGE OUR PARTNER SECTION */}
+        <div className="new-our-partner">
+            <h1 className="section-title mt-3 text-center">RECOGNITIONS & PARTNERSHIPS</h1>
+            <OurPartner />
+        </div>
+
+        {/* NEW HOMEPAGE CLIENT SAY */}
+        <Suspense fallback={<div>Loading...</div>}>
+            <ClientSay />
+        </Suspense>
 
         {/* NEW HOMEPAGE CAREER SECTION */}
 
-        <div className="new-career-section" style={{width:"auto", height:"auto", backgroundColor:"#19AF8F", padding:"30px"}}>
+        <div className="new-career-section" style={{ width: "auto", height: "auto", backgroundColor: "#19AF8F", padding: "30px" }}>
 
-                <div className="new-career-section new-all-margin">
-        
+            <div className="new-career-section new-all-margin">
+
                 <div className="career-section-row row g-0">
                     <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 career-caption">
                         <h5>WORK WITH US</h5>
                         <h2>Let's Grow Together</h2>
                     </div>
                     <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 career-details">
-                        <p>We are on a mission to solve complex problems and provide solutions 
+                        <p>We are on a mission to solve complex problems and provide solutions
                             that make a difference, Come work with us.
                         </p>
 
@@ -158,13 +158,13 @@ function Test (){
         <div id="consent-popup" className="hidden cookies-banner">
             <div className="row">
                 <div className="col-xl-8 col-lg-8">
-                <p>This website uses cookies. We use cookies to optimize web functionality, collect website analytics and traffic data, and to provide a more personalized user experience.
-                    <a className="cookies-readmore-link" href="/cookiespolicy">Read more...</a> 
-            </p>
+                    <p>This website uses cookies. We use cookies to optimize web functionality, collect website analytics and traffic data, and to provide a more personalized user experience.
+                        <a className="cookies-readmore-link" href="/cookiespolicy">Read more...</a>
+                    </p>
                 </div>
                 <div className="col-xl-4 col-lg-4 text-center">
-                <button className="btn btn-default cookies-decline-btn" id="decline" >Decline</button>
-                <button className="btn btn-default cookies-accept-btn" id="accept" >Accept</button>
+                    <button className="btn btn-default cookies-decline-btn" id="decline" >Decline</button>
+                    <button className="btn btn-default cookies-accept-btn" id="accept" >Accept</button>
                 </div>
             </div>
         </div>
