03d7bc6bde79b2a59af0ca195759c088f3e477bb
Manikanta Kumar Nalam <manikanta@amnetdigital.com>
2022-11-09 12:39:02 +0530

    color changed for ribbon

--- src/assets/components/Webinars/Webinarmodal.jsx
+++ src/assets/components/Webinars/Webinarmodal.jsx
@@ -35,17 +35,21 @@ const Webinarmodal = ({ isShow, closeModal }) => {
               11:30 AM - 12:15 PM EST, Thursday, November 17, 2022
             </p>
             <div className="mt-3-rem  w-100">
-              <p className="w-50 mx-auto">
-                <Link
-                  onClick={storeData}
-                  to={`/webinar-reshaping-the-retail-industry-through-AI`}
-                  className="fs-10 d-line-block pl-1 link-decoration-none"
-                  target="_blank"
-                >
-                  <span className="d-inline-block w-100 submit-button clr-orange cr-pointer px-4">
-                    Register now
-                  </span>
-                </Link>
+              <p className="row">
+                <div className="col-md-3 col-lg-3 col-sm-2 col-xs-2"></div>
+                <div className="col-md-6 col-lg-6 col-sm-8 col-xs-8">
+                  <Link
+                    onClick={storeData}
+                    to={`/webinar-reshaping-the-retail-industry-through-AI`}
+                    className="fs-10 d-line-block pl-1 link-decoration-none"
+                    target="_blank"
+                  >
+                    <span className="d-inline-block w-100 submit-button clr-orange cr-pointer px-4">
+                      Register now
+                    </span>
+                  </Link>
+                </div>
+                <div className="col-md-3 col-lg-3 col-sm-2 col-xs-2"></div>
               </p>
             </div>
           </div>
