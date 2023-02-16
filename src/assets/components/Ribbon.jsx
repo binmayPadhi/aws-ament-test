c35d52f3ced8f98bd6be811446df4662e1534db8
Manikanta Kumar Nalam <manikanta@amnetdigital.com>
2023-02-16 20:26:26 +0530

    updated ribbon changes

--- src/assets/components/Ribbon.jsx
+++ src/assets/components/Ribbon.jsx
@@ -12,34 +12,36 @@ const Ribbon = ({ checkRibbon }) => {
   return (
     <>
       <div className="ribbonBackground py-4">
-        <div className="w-95 mx-auto">
-          <div className="d-flex w-100 justify-content-between align-items-center">
-            <p></p>
-            <p className="mb-0 px-4">
-              <img src={speaker} alt="speaker" className="speaker_width" />
-              <span className="ribbon_content px-2 mb-0">
-                On-Demand Webinar - See how leading Retailers are embracing AI &
-                Analytics to win in 2023
-              </span>
-              <span className="btn-ribbon px-3 py-2 mt-1 mb-0">
-                <Link
-                  to="/webinar-reshaping-the-retail-industry-through-AI"
-                  target="_blank"
-                  className="text-dark"
-                >
-                  Watch now
-                </Link>
-              </span>
-            </p>
-            <div className="pr-4">
-              <p className="mb-0 d-flex justify-content-md-end justify-content-lg-end justify-content-center">
+        <div className="w-98 mx-auto">
+          <div className="row mx-0">
+            <div className="col-md-2 col-lg-2 col-sm-12"></div>
+            <div className="col-md-8 col-lg-8 col-sm-12">
+              <p className="mb-0 text-center mt-2">
+                <img src={speaker} alt="speaker" className="speaker_width" />
+                <span className="ribbon_content px-2 mb-0">
+                  On-Demand Webinar - See how leading Retailers are embracing AI
+                  & Analytics to win in 2023
+                </span>
+                <span className="btn-ribbon px-3 py-2 mt-1 mb-0">
+                  <Link
+                    to="/webinar-reshaping-the-retail-industry-through-AI"
+                    target="_blank"
+                    className="text-dark"
+                  >
+                    Watch now
+                  </Link>
+                </span>
+              </p>
+            </div>
+            <div className="col-md-2 col-lg-2 col-sm-12">
+              <div className="float-md-right">
                 <div
                   className="text-center close_icon ml-3"
                   onClick={(e) => close(e)}
                 >
                   x
                 </div>
-              </p>
+              </div>
             </div>
           </div>
         </div>
