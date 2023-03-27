47d45ab6720f55cfdde8b117f4b79f6673d6a8bb
Manikanta Kumar Nalam <manikanta@amnetdigital.com>
2023-03-27 19:42:55 +0530

    carrers page is completed

--- src/assets/Data/AboutUs-Data/About.js
+++ src/assets/Data/AboutUs-Data/About.js
@@ -8,150 +8,172 @@ import vision from "../../images/About-Us-Page/vision.png";
 import integrity from "../../images/About-Us-Page/Integrity.png";
 import Excellence from "../../images/About-Us-Page/Excellence.png";
 import Empathy from "../../images/About-Us-Page/Empathy.png";
-import jan from '../../images/About-Us-Page/January.png';
-import feb from '../../images/About-Us-Page/February.png';
-import nov from '../../images/About-Us-Page/November.png';
-import oct from '../../images/About-Us-Page/October.png';
-import dec from '../../images/About-Us-Page/December.png';
-import mar from '../../images/About-Us-Page/March.png';
-import orange from '../../images/About-Us-Page/Orange.png';
-import violet from '../../images/About-Us-Page/violet.png';
-import skyblue from '../../images/About-Us-Page/skyblue.png';
-import yellow from '../../images/About-Us-Page/yellow.png';
-import blue from '../../images/About-Us-Page/blue.png';
-import green from '../../images/About-Us-Page/green.png';
+import jan from "../../images/About-Us-Page/January.png";
+import feb from "../../images/About-Us-Page/February.png";
+import nov from "../../images/About-Us-Page/November.png";
+import oct from "../../images/About-Us-Page/October.png";
+import dec from "../../images/About-Us-Page/December.png";
+import mar from "../../images/About-Us-Page/March.png";
+import orange from "../../images/About-Us-Page/Orange.png";
+import violet from "../../images/About-Us-Page/violet.png";
+import skyblue from "../../images/About-Us-Page/skyblue.png";
+import yellow from "../../images/About-Us-Page/yellow.png";
+import blue from "../../images/About-Us-Page/blue.png";
+import green from "../../images/About-Us-Page/green.png";
 import innov from "../../images/About-Us-Page/innov.png";
+import impact from "../../images/careers-page/impact.png";
+import development from "../../images/careers-page/development.png";
+import progress from "../../images/careers-page/progress.png";
+import support from "../../images/careers-page/support.png";
 
 const Aboutdata = {
-    whoweare: [
-        {
-            id: 1,
-            image: expertise,
-            heading: "Expertise",
-            description: "We’re AI & Analytics tech experts with deep experience in solving complex business challenges for Fortune 1000 companies."
-        },
-        {
-            id: 2,
-            image: innovationnew,
-            heading: "Innovation",
-            description: "We're innovating in technology space, leveraging cutting-edge digital frameworks, tech accelerators, and labs."
-        },
-        {
-            id: 3,
-            image: reliability,
-            heading: "Reliability",
-            description: "We commit and deliver what we promise without negotiating on the quality of your business application or software product."
-        },
-        {
-            id: 4,
-            image: trust,
-            heading: "Trust",
-            description: "We’re your trusted AI and Data analytics service partner by adhering to industry best technology practices."
-      }
-    ],
-    missionblock: [
-        {
-            id: 1,
-            image: mission,
-            heading: 'Our Mission',
-            description: "At Amnet Digital, our mission is to help businesses build and adapt a data-driven culture by creating innovative end-to-end digital solutions. With a unique blend of AI and Data Analytics, we dedicate our business process, technology innovation know-how, and deep industry expertise and resources to assist modern businesses going beyond."
-        },
-        {
-            id: 2,
-            image: vision,
-            heading: 'Our Vision',
-            description: "Our brand vision is to assist customers to achieve their business goals along with better futures for communities and the planet. Transform traditional and emerging businesses with AI and Data Analytics to generate meaningful value through our AI-driven services and solutions."
-        }
-    ],
-    aboutvalues: [
-        {
-            id: 1,
-            image: innovating,
-            heading: 'Innovation',
-            description: "We unleash creativity by making innovation part of each associate’s job responsibility"
-        },
-        {
-            id: 2,
-            image: integrity,
-            heading: 'Integrity',
-            description: "We strive towards doing the right thing, at the right time for our customers, partners and teams"
-        },
-        {
-            id: 3,
-            image: Excellence,
-            heading: 'Excellence',
-            description: "We challenge the status quo and opinions to build premier products and deliver the best services"
-        },
-        {
-            id: 4,
-            image: Empathy,
-            heading: 'Empathy',
-            description: "We are curious and take leaps to understand and respect other’s perspectives for success"
-        },
-
-    ],
-    ourachivements: [
-        {
-            id: 1,
-            image: jan,
-            flag: orange,
-            heading: "January 2023",
-            description: " As we are growing with every passing year both in revenue and associates, we had moved to a bigger and better office space."
-        },
-        {
-            id: 2,
-            image: nov,
-            flag: violet,
-            heading: "November 2022",
-            description: "Expanded our business operations by entering into the Dubai (UAE) emerging market. Servicing our customers across the globe with a team size of 250+ technology consultants from our 3 other operating centers."
-        },
-        {
-            id: 3,
-            image: oct,
-            flag: skyblue,
-            heading: "October 2022",
-            description: "Launched our first product, Swift Insights - An AI-based Advanced Analytics dashboards platform to empower Retail and eCommerce businesses to unpack key insights to make well-informed business decisions."
-        },
-        {
-            id: 4,
-            image: dec,
-            flag: yellow,
-            heading: "December 2021",
-             description: "Achieved a milestone of hiring 100+ technology consultants across the globe to service our customers as part of our business expansion strategy."
-        },
-        {
-            id: 5,
-            image: mar,
-            flag: blue,
-            heading: "March 2020",
-             description: "We have won our first three paying customers from the list of Fortune 1000 companies. We have been felicitated as one of the best startups to work in Hyderabad by Silicon India magazine."
-        },
-        {
-            id: 6,
-            image: feb,
-            flag: green,
-            heading: "February 2019",
-             description: "Initially we started as Amnet LLC in U.S but with time we officially registered as Amnet Digital. And  we launched our full fledged operations center in Hyderabad, India."
-        }
-    ],
-    clientsay: [
-        {
-            id: 1,
-            heading: "Director of Data Science, Retail Marketplace",
-            description: "The assistance we got from Amnet Digital was the glue that kept together our transformation initiatives for FY 2019-20. They stayed on top of our challenging unstructured data and provided world-class data analysis. We were able to unearth hidden relationships in our customers buying patterns for better forecast and bundling of products."
-        },
-        {
-            id: 2,
-            heading: "VP Engineering, Information Management Company",
-            description: "We developed a great partnership with Amnet Digital and their dedication towards our digital transformation was commendable. We appreciate their attention to detail, modernization approach, incorporation of machine learning & automation of age-old and time-intensive documentation & data deduplication processes."
-        },
-        {
-            id: 3,
-            heading: "VP Engineering, eCommerce Company",
-            description: "We would like to express our satisfaction regarding the development of our eCommerce web application. Amnet's application engineering team did a very professional job. We are satisfied with the solution given to us and with the communication flow through the project. We look forward to working with them in future projects."
-        }
-    ],
-    dropdownlist: [
+  whoweare: [
+    {
+      id: 1,
+      image: expertise,
+      heading: "Expertise",
+      description:
+        "We’re AI & Analytics tech experts with deep experience in solving complex business challenges for Fortune 1000 companies.",
+    },
+    {
+      id: 2,
+      image: innovationnew,
+      heading: "Innovation",
+      description:
+        "We're innovating in technology space, leveraging cutting-edge digital frameworks, tech accelerators, and labs.",
+    },
+    {
+      id: 3,
+      image: reliability,
+      heading: "Reliability",
+      description:
+        "We commit and deliver what we promise without negotiating on the quality of your business application or software product.",
+    },
+    {
+      id: 4,
+      image: trust,
+      heading: "Trust",
+      description:
+        "We’re your trusted AI and Data analytics service partner by adhering to industry best technology practices.",
+    },
+  ],
+  missionblock: [
+    {
+      id: 1,
+      image: mission,
+      heading: "Our Mission",
+      description:
+        "At Amnet Digital, our mission is to help businesses build and adapt a data-driven culture by creating innovative end-to-end digital solutions. With a unique blend of AI and Data Analytics, we dedicate our business process, technology innovation know-how, and deep industry expertise and resources to assist modern businesses going beyond.",
+    },
+    {
+      id: 2,
+      image: vision,
+      heading: "Our Vision",
+      description:
+        "Our brand vision is to assist customers to achieve their business goals along with better futures for communities and the planet. Transform traditional and emerging businesses with AI and Data Analytics to generate meaningful value through our AI-driven services and solutions.",
+    },
+  ],
+  aboutvalues: [
+    {
+      id: 1,
+      image: innovating,
+      heading: "Innovation",
+      description:
+        "We unleash creativity by making innovation part of each associate’s job responsibility",
+    },
+    {
+      id: 2,
+      image: integrity,
+      heading: "Integrity",
+      description:
+        "We strive towards doing the right thing, at the right time for our customers, partners and teams",
+    },
+    {
+      id: 3,
+      image: Excellence,
+      heading: "Excellence",
+      description:
+        "We challenge the status quo and opinions to build premier products and deliver the best services",
+    },
+    {
+      id: 4,
+      image: Empathy,
+      heading: "Empathy",
+      description:
+        "We are curious and take leaps to understand and respect other’s perspectives for success",
+    },
+  ],
+  ourachivements: [
+    {
+      id: 1,
+      image: jan,
+      flag: orange,
+      heading: "January 2023",
+      description:
+        " As we are growing with every passing year both in revenue and associates, we had moved to a bigger and better office space.",
+    },
+    {
+      id: 2,
+      image: nov,
+      flag: violet,
+      heading: "November 2022",
+      description:
+        "Expanded our business operations by entering into the Dubai (UAE) emerging market. Servicing our customers across the globe with a team size of 250+ technology consultants from our 3 other operating centers.",
+    },
+    {
+      id: 3,
+      image: oct,
+      flag: skyblue,
+      heading: "October 2022",
+      description:
+        "Launched our first product, Swift Insights - An AI-based Advanced Analytics dashboards platform to empower Retail and eCommerce businesses to unpack key insights to make well-informed business decisions.",
+    },
+    {
+      id: 4,
+      image: dec,
+      flag: yellow,
+      heading: "December 2021",
+      description:
+        "Achieved a milestone of hiring 100+ technology consultants across the globe to service our customers as part of our business expansion strategy.",
+    },
+    {
+      id: 5,
+      image: mar,
+      flag: blue,
+      heading: "March 2020",
+      description:
+        "We have won our first three paying customers from the list of Fortune 1000 companies. We have been felicitated as one of the best startups to work in Hyderabad by Silicon India magazine.",
+    },
+    {
+      id: 6,
+      image: feb,
+      flag: green,
+      heading: "February 2019",
+      description:
+        "Initially we started as Amnet LLC in U.S but with time we officially registered as Amnet Digital. And  we launched our full fledged operations center in Hyderabad, India.",
+    },
+  ],
+  clientsay: [
+    {
+      id: 1,
+      heading: "Director of Data Science, Retail Marketplace",
+      description:
+        "The assistance we got from Amnet Digital was the glue that kept together our transformation initiatives for FY 2019-20. They stayed on top of our challenging unstructured data and provided world-class data analysis. We were able to unearth hidden relationships in our customers buying patterns for better forecast and bundling of products.",
+    },
+    {
+      id: 2,
+      heading: "VP Engineering, Information Management Company",
+      description:
+        "We developed a great partnership with Amnet Digital and their dedication towards our digital transformation was commendable. We appreciate their attention to detail, modernization approach, incorporation of machine learning & automation of age-old and time-intensive documentation & data deduplication processes.",
+    },
+    {
+      id: 3,
+      heading: "VP Engineering, eCommerce Company",
+      description:
+        "We would like to express our satisfaction regarding the development of our eCommerce web application. Amnet's application engineering team did a very professional job. We are satisfied with the solution given to us and with the communication flow through the project. We look forward to working with them in future projects.",
+    },
+  ],
+  dropdownlist: [
     {
       id: 30,
       name: "",
@@ -202,7 +224,41 @@ const Aboutdata = {
       name: "Quality Assurance",
       link: "",
     },
-  ]
-}
+  ],
+  corevalues: [
+    {
+      id: 100,
+      selectedName: "impact",
+      image: impact,
+      heading: "Proven track record",
+      description:
+        "4+ years, built 10+ AI-based Digital Solutions, and successfully servicing Fortune 1000 companies.",
+    },
+    {
+      id: 101,
+      selectedName: "development",
+      image: development,
+      heading: "We are doers",
+      description:
+        "We do whatever it takes in building your dream MVP in first 90 days. Our only focus is always on tech execution excellence.",
+    },
+    {
+      id: 102,
+      selectedName: "progress",
+      image: progress,
+      heading: "We are doers",
+      description:
+        "We always hire the best-in-class technology consultants and specialists with great minds to solve the most complex technology challenges.",
+    },
+    {
+      id: 103,
+      selectedName: "support",
+      image: support,
+      heading: "Happy customers",
+      description:
+        "Empowering our customers to achieve their business goals consistently leveraging our deep technology experience without negotiating on the quality.",
+    },
+  ],
+};
 
 export default Aboutdata;
--- src/assets/components/Careers/WeValueInnovation.jsx
+++ src/assets/components/Careers/WeValueInnovation.jsx
@@ -1,35 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Whyamnet from "../../images/careers-page/whyament.png";
+import valuesfrom from "../../Data/AboutUs-Data/About";
 
-function WeValueInnovation (){
-    return (
-        <>
-            <div className="weValueInnovation">
-                <div className="container">
-            <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-12">
-                   <h1 className="text-white amnet-styling ">Why Amnet Digital</h1>
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-12 d-flex">      
-                 <h2 className="text-white impact-space ">Impact<hr className="hr-styling"/></h2>
-                 <h2 className="text-white impact-space">Development <hr/></h2>
-                 <h2 className="text-white impact-space">Progress <hr/></h2>
-                 <h2 className="text-white impact-space">Support<hr/></h2>   
-                </div>
+function WeValueInnovation() {
+  const [selectedObj, replaceObject] = useState(valuesfrom.corevalues[0]);
+  const [selectedName, changeSelectedname] = useState(
+    valuesfrom.corevalues[0].selectedName
+  );
+  const chooseData = (e, selectedData) => {
+    e.preventDefault();
+    replaceObject(selectedData);
+    changeSelectedname(selectedData.selectedName);
+  };
+  return (
+    <>
+      <div className="weValueInnovation">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              <h1 className="text-white amnet-styling ">Why Amnet Digital</h1>
             </div>
-            <div className="row">
-                <div className="col-lg-6 col-md-6 col-sm-12">
-            <img className="d-block w-100 wevalue-img" src={Whyamnet} alt="First slide"/>
-                </div>
-                <div className="col-lg-6 col-md-6 col-sm-12 my-auto">
-                     <h1 className="proven-styling">Proven track record</h1>
-                     <h1 className="years-styling mt-4">4+ years, built 10+ AI-based Digital Solutions, and successfully servicing Fortune 1000 companies.</h1>
-                </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 d-flex">
+              <ul>
+                {valuesfrom.corevalues.map((res) => {
+                  return (
+                    <li
+                      key={res.id}
+                      className={
+                        "text-white impact-space d-inline-block cr-pointer px-3 py-2 border-grey " +
+                        (res["selectedName"] === selectedName
+                          ? "border-white"
+                          : "border-grey")
+                      }
+                      onClick={(e) => chooseData(e, res)}
+                    >
+                      {res.selectedName}
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12">
+              <img
+                className="d-block w-100 wevalue-img"
+                src={selectedObj.image}
+                alt="First slide"
+              />
             </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 my-auto">
+              <h1 className="proven-styling">{selectedObj.heading}</h1>
+              <h1 className="years-styling mt-4">{selectedObj.description}</h1>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default WeValueInnovation;
+
+{
+  /* <h2 className="text-white impact-space ">Impact</h2>
+              <h2 className="text-white impact-space">Development</h2>
+              <h2 className="text-white impact-space">Progress</h2>
+              <h2 className="text-white impact-space">Support</h2> */
+}
