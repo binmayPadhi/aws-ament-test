4fa421c3ebbb2c86af095f25fe5cf4662bb0e7f4
Manikanta Kumar Nalam <manikanta@amnetdigital.com>
2023-05-30 20:03:50 +0530

    case studies completed

--- src/assets/components/SubServices/Whyamnet.jsx
+++ src/assets/components/SubServices/Whyamnet.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import image1 from "../../images/Service-Page/bar.png";
-import image2 from "../../images/Service-Page/plane.png";
-import image3 from "../../images/Service-Page/hand.png";
-import image4 from "../../images/Service-Page/smile.png";
+import image1 from "../../images/HomePage/bar.png";
+import image2 from "../../images/HomePage/plane.png";
+import image3 from "../../images/HomePage/hand.png";
+import image4 from "../../images/HomePage/smile.png";
 
 const Whyamnet = () => {
   return (
     <div className="row pb-3 pt-5">
       <div className="col-md-6 col-lg-5 col-xs-12 col-sm-12 border-col1 mb-5">
-        <div className="company_name text-center">
-          Why <br />
-          Amnet Digital?
-        </div>
+        <p className="text-left glance_styles glances_position">
+          Why
+          <p className="text-right glance_styles">
+            Amnet Digital?
+          </p>
+        </p>
       </div>
-      <div className="col-md-6 col-lg-7 col-xs-12 col-sm-12 mt-15">
+      <div className="col-md-6 col-lg-6 col-xs-12 col-sm-12 mt-5 mb-5">
         <div className="w-100 d-flex text-left">
           <span>
             <img src={image1} />
@@ -21,8 +23,7 @@ const Whyamnet = () => {
           <span>
             <p className="heading_why pl-4 mb-0">Proven track record</p>
             <p className="paragraph_why pl-4 width_query text-justify">
-              6+ years, built 100+ AI-based Digital Solutions, and successfully
-              servicing Fortune 1000 companies.
+              4+ years, built 10+ AI-based digital solutions, and successfully servicing fortune 1000 companies.
             </p>
           </span>
         </div>
@@ -31,9 +32,7 @@ const Whyamnet = () => {
             <p className="heading_why pr-4 mb-0">We are doers</p>
             <p className="w-100 justify-content-end d-flex">
               <p className="paragraph_why pr-4 width_query text-justify">
-                We do whatever it takes in building your dream MVP in first 90
-                days to handling 1 billion hits a day. Our only focus is always
-                on tech execution excellence.
+              We do whatever it takes in building your dream MVP in first 90 days. Our only focus is always on tech execution excellence.
               </p>
             </p>
           </span>
@@ -48,9 +47,7 @@ const Whyamnet = () => {
           <span>
             <p className="heading_why pl-4 mb-0">Awesome talent</p>
             <p className="paragraph_why pl-4 width_query text-justify">
-              We always hire the best-in-class technology consultants and
-              specialists with great minds to solve the most complex technology
-              challenges.
+            We always hire the world-class technology consultants with great minds to solve the most complex technology challenges.
             </p>
           </span>
         </div>
@@ -59,9 +56,7 @@ const Whyamnet = () => {
             <p className="heading_why pr-4 mb-0">Happy customers</p>
             <p className="w-100 justify-content-end d-flex">
               <p className="paragraph_why pr-4 width_query text-justify">
-                Empowering our customers to achieve their business goals
-                consistently leveraging our combined 100+ years of technology
-                experience without negotiating on the quality.
+              Empowering our customers to achieve their business goals consistently leveraging our deep technology experience without negotiating on the quality.
               </p>
             </p>
           </span>
